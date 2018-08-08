50938440df0d93b16d86f694201f66e2f35275a0
JonaOlmedo <39449135+JonaOlmedo@users.noreply.github.com>
2018-08-07 23:34:49 -0600

    Menu
    menu

--- app/views/dashboard/menu/menu_index.php
+++ app/views/dashboard/menu/menu_index.php
@@ -1,41 +1,41 @@
-<div class="ingresar_fondo">
-<div class="row">
-<div class="col s12 m2 l6 offset-l3">
-<ul class="collection">
-    <li class="collection-item avatar">
-      <img src="images/yuna.jpg" alt="" class="circle">
-      <span class="title">Title</span>
-      <p>First Line <br>
-         Second Line
-      </p>
-      <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
-    </li>
-    <li class="collection-item avatar">
-      <i class="material-icons circle">folder</i>
-      <span class="title">Title</span>
-      <p>First Line <br>
-         Second Line
-      </p>
-      <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
-    </li>
-    <li class="collection-item avatar">
-      <i class="material-icons circle green">insert_chart</i>
-      <span class="title">Title</span>
-      <p>First Line <br>
-         Second Line
-      </p>
-      <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
-    </li>
-    <li class="collection-item avatar">
-      <i class="material-icons circle red">play_arrow</i>
-      <span class="title">Title</span>
-      <p>First Line <br>
-         Second Line
-      </p>
-      <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
-    </li>
-  </ul>
-            
+<!doctype html>
+<html lang=''>
+<head>
+   <meta charset='utf-8'>
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="styles.css">
+   <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+   <script src="script.js"></script>
+   <title>CSS MenuMaker</title>
+</head>
+<body>
+
+<div id='cssmenu'>
+<ul>
+   <li><a href='#'>Home</a></li>
+   <li class='active has-sub'><a href='#'>Products</a>
+      <ul>
+         <li class='has-sub'><a href='#'>Product 1</a>
+            <ul>
+               <li><a href='#'>Sub Product</a></li>
+               <li><a href='#'>Sub Product</a></li>
+            </ul>
+         </li>
+         <li class='has-sub'><a href='#'>Product 2</a>
+            <ul>
+               <li><a href='#'>Sub Product</a></li>
+               <li><a href='#'>Sub Product</a></li>
+            </ul>
+         </li>
+      </ul>
+   </li>
+   <li><a href='#'>About</a></li>
+   <li><a href='#'>Contact</a></li>
+</ul>
 </div>
-</div>
-</div>
+
+</body>
+<html>
+
+<img src="../../web/img/login/fondo.jpg" border="1" alt="Este es el ejemplo de un texto alternativo" width="400" height="300">
--- app/views/dashboard/templates/page.class.php
+++ app/views/dashboard/templates/page.class.php
@@ -29,6 +29,7 @@ class Page extends component{
         </main>
         <script type='text/javascript' src='../../web/js/jquery-3.2.1.min.js'></script>
         <script type='text/javascript' src='../../web/js/materialize.min.js'></script>
+        <script type='text/javascript' src='../../web/js/script.js'></script>
 		</body>
 		</html>
         ");
