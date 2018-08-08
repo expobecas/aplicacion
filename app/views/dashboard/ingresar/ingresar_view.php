3819b21f16e20b7380231f66c8b8208cb465b21a
expobecas <37518924+expobecas@users.noreply.github.com>
2018-08-07 22:02:32 -0600

    LOGITO

--- app/views/dashboard/ingresar/ingresar_view.php
+++ app/views/dashboard/ingresar/ingresar_view.php
@@ -3,7 +3,7 @@
     <form method='post' class="col s12 m8 l4 push-l4 cuadrado push-m2 z-depth-5  ingresar_form">
     <!--<h5 class="white-text">Acceder</h5>-->
       <div class="row">
-      <img class="logo" src="../../../web/img/logo_ricaldone.png" alt="logo">
+      <img class="logo" src="../../web/img/logo_ricaldone.png" alt="logo">
         <div class="input-field col s12">
           <input id='usuario' type="text" name='usuario' class="validate">
           <label for='usuario'>Usuario</label>
