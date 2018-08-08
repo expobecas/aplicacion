2903f42f800bf846aeacc5ced8a5a2c548db1769
expobecas <37518924+expobecas@users.noreply.github.com>
2018-08-07 22:00:44 -0600

    RICK

---
+++ dashboard/ingresar/ingresar.php
@@ -0,0 +1,6 @@
+<?php
+require_once("../../app/views/dashboard/templates/page.login.php");
+Page::templateHeader("Ingresar");
+require_once("../../app/views/dashboard/ingresar/ingresar_view.php");
+Page::templateFooter();
+?>
