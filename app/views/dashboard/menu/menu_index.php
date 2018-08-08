048fadd06ccedcd6db62f12ec833fa5b3e1359b3
JonaOlmedo <39449135+JonaOlmedo@users.noreply.github.com>
2018-08-07 23:08:51 -0600

    Hola
    hola

--- app/views/dashboard/menu/menu_index.php
+++ app/views/dashboard/menu/menu_index.php
@@ -1,36 +1,41 @@
 <div class="ingresar_fondo">
 <div class="row">
-<div class="col s12 m2 l6 offset-l4">
-<div class="col s12 m7">
-    <h2 class="header">Casos</h2>
-    <div class="card horizontal">
-      <div class="card-image">
-        <img src="../../web/img/caso.png">
-      </div>
-      <div class="card-stacked">
-        <div class="card-action ">
-          <a href="#">Entrar</a>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-</div>
-<div class="row">
-<div class="col s12 m2 l6 offset-l4">
-<div class="col s12 m7">
-    <h2 class="header">Citas</h2>
-    <div class="card horizontal ">
-      <div class="card-image">
-        <img src="../../web/img/cita.jpg">
-      </div>
-      <div class="card-stacked">
-        <div class="card-action">
-          <a href="#">Entrar</a>
-        </div>
-      </div>
-    </div>
-  </div>
+<div class="col s12 m2 l6 offset-l3">
+<ul class="collection">
+    <li class="collection-item avatar">
+      <img src="images/yuna.jpg" alt="" class="circle">
+      <span class="title">Title</span>
+      <p>First Line <br>
+         Second Line
+      </p>
+      <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+    </li>
+    <li class="collection-item avatar">
+      <i class="material-icons circle">folder</i>
+      <span class="title">Title</span>
+      <p>First Line <br>
+         Second Line
+      </p>
+      <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+    </li>
+    <li class="collection-item avatar">
+      <i class="material-icons circle green">insert_chart</i>
+      <span class="title">Title</span>
+      <p>First Line <br>
+         Second Line
+      </p>
+      <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+    </li>
+    <li class="collection-item avatar">
+      <i class="material-icons circle red">play_arrow</i>
+      <span class="title">Title</span>
+      <p>First Line <br>
+         Second Line
+      </p>
+      <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>
+    </li>
+  </ul>
+            
 </div>
 </div>
 </div>
