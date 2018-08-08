c4641357d6bd8a12506b2959d400f8dd86ff743f
expobecas <37518924+expobecas@users.noreply.github.com>
2018-08-07 21:40:18 -0600

    Ñeeeeeeee

---
+++ app/views/ingresar/ingresar_view.php
@@ -0,0 +1,20 @@
+<div class="ingresar_fondo">
+<div class="row">
+    <form method='post' class="col s12 m8 l4 push-l4 cuadrado push-m2 z-depth-5  ingresar_form">
+    <!--<h5 class="white-text">Acceder</h5>-->
+      <div class="row">
+      <img class="logo" src="../../../web/img/logo_ricaldone.png" alt="logo">
+        <div class="input-field col s12">
+          <input id='usuario' type="text" name='usuario' class="validate">
+          <label for='usuario'>Usuario</label>
+        </div>
+        <div class="input-field col s12">
+          <input id="password" type="password" name='clave' class="validate">
+          <label for="password">Contraseña</label>
+        </div>
+      </div>
+      <button class="waves-effect waves-light btn ingresar" type='submit' name='ingresar'>Ingresar</button>      
+      </div>
+    </form>
+  </div>
+  </div>
--- app/views/templates/page.login.php
+++ app/views/templates/page.login.php
@@ -1,7 +1,6 @@
 <?php 
-require_once("../../app/helpers/validator.class.php");
-require_once("../../app/helpers/component.class.php");
-require_once("../../app/models/database.class.php");
+require_once("../../../app/helpers/validator.class.php");
+require_once("../../../app/helpers/component.class.php");
 class Page extends component{
     public static function templateHeader($title){
         session_start();
@@ -12,11 +11,11 @@ class Page extends component{
         <head>
             <meta charset='utf-8'>
             <title>$title</title>
-            <link type='text/css' rel='stylesheet' href='../../web/css/materialize.min.css'/>
-            <link type='text/css' rel='stylesheet' href='../../web/css/material_icons.css'/>
-            <link type='text/css' rel='stylesheet' href='../../web/css/material_icons.css'/>
-            <link type='text/css' rel='stylesheet' href='../../web/css/style_alumno.css'/>
-            <script type='text/javascript' src='../../web/js/sweetalert.min.js'></script>
+            <link type='text/css' rel='stylesheet' href='../../../web/css/materialize.min.css'/>
+            <link type='text/css' rel='stylesheet' href='../../../web/css/material_icons.css'/>
+            <link type='text/css' rel='stylesheet' href='../../../web/css/material_icons.css'/>
+            <link type='text/css' rel='stylesheet' href='../../../web/css/style_alumno.css'/>
+            <script type='text/javascript' src='../../../web/js/sweetalert.min.js'></script>
             <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
         </head>
         <body>
@@ -27,8 +26,8 @@ class Page extends component{
     public static function templateFooter(){
         print("
         </main>
-        <script type='text/javascript' src='../../web/js/jquery-3.2.1.min.js'></script>
-        <script type='text/javascript' src='../../web/js/materialize.min.js'></script>
+        <script type='text/javascript' src='../../../web/js/jquery-3.2.1.min.js'></script>
+        <script type='text/javascript' src='../../../web/js/materialize.min.js'></script>
 		</body>
 		</html>
         ");
