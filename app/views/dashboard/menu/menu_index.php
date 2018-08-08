81a1e4ed404b8744a70aa4494fbfdd599c854579
expobecas <37518924+expobecas@users.noreply.github.com>
2018-08-07 23:09:31 -0600

    neles

--- app/views/dashboard/menu/menu_index.php
+++ app/views/dashboard/menu/menu_index.php
@@ -2,7 +2,6 @@
 <div class="row">
 <div class="col s12 m2 l6 offset-l4">
 <div class="col s12 m7">
-    <h2 class="header">Casos</h2>
     <div class="card horizontal">
       <div class="card-image">
         <img src="../../web/img/caso.png">
@@ -19,7 +18,6 @@
 <div class="row">
 <div class="col s12 m2 l6 offset-l4">
 <div class="col s12 m7">
-    <h2 class="header">Citas</h2>
     <div class="card horizontal ">
       <div class="card-image">
         <img src="../../web/img/cita.jpg">
