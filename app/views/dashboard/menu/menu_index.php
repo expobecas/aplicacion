61d618c480a9a91ed8186b75e6c58feb931cee7a
JonaOlmedo <39449135+JonaOlmedo@users.noreply.github.com>
2018-08-07 23:45:33 -0600

    menu
    menu

--- app/views/dashboard/menu/menu_index.php
+++ app/views/dashboard/menu/menu_index.php
@@ -12,25 +12,21 @@
 
 <div id='cssmenu'>
 <ul>
-   <li><a href='#'>Home</a></li>
-   <li class='active has-sub'><a href='#'>Products</a>
+   <li class='active has-sub'><a href='#'>Opciones de administracion</a>
       <ul>
-         <li class='has-sub'><a href='#'>Product 1</a>
+         <li class='has-sub'><a href='#'>Citas</a>
             <ul>
-               <li><a href='#'>Sub Product</a></li>
-               <li><a href='#'>Sub Product</a></li>
+               <li><a href='#'>Ver</a></li>
             </ul>
          </li>
-         <li class='has-sub'><a href='#'>Product 2</a>
+         <li class='has-sub'><a href='#'>Casos</a>
             <ul>
-               <li><a href='#'>Sub Product</a></li>
-               <li><a href='#'>Sub Product</a></li>
+               <li><a href='#'>Ver</a></li>
             </ul>
          </li>
       </ul>
    </li>
-   <li><a href='#'>About</a></li>
-   <li><a href='#'>Contact</a></li>
+   <li><a href='#'>Cerrar sesion</a></li>
 </ul>
 </body>
 <html>
