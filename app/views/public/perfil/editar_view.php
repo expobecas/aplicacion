0e6a11910117895661ff5e01aad2ea19867b476e
expobecas <37518924+expobecas@users.noreply.github.com>
2018-08-08 22:10:58 -0600

    editar perfil - vista

---
+++ app/views/public/perfil/editar_view.php
@@ -0,0 +1,62 @@
+<!--FORMULARIO PARA EDICIÓN DE PERFIL-->
+<div class="form-edit">
+<div class="row">
+   <form class="col s10 l6 offset-l4 offset-s1 white edit_form" method='POST'>
+      <p class="titulo-EP"><strong>Editar Perfil</strong></p>
+      <div class="divider"></div>
+      <!--PRIMERA FILA-->
+      <div class="row">
+         <div class="input-field col s6">
+            <input id="nombre1" type="text" class="validate" name='nombre1'>
+            <label for="nombre1">Primer nombre</label>
+         </div>
+         <div class="input-field col s6">
+            <input id="nombre2" type="text" class="validate" name='nombre2'>
+            <label for="nombre2">Segundo nombre</label>
+         </div>
+      </div>
+      <!--SEGUNDA FILA-->
+      <div class="row">
+         <div class="input-field col s6">
+            <input id="apellido1" type="text" class="validate" name='apellido1'>
+            <label for="apellido1">Primer apellido</label>
+         </div>
+         <div class="input-field col s6">
+            <input id="apellido2" type="text" class="validate" name='apellido2'>
+            <label for="apellido2">Segundo apellido</label>
+         </div>
+         
+      </div>
+      <!--TERCERA FILA-->
+      <div class="row">
+         <div class="input-field col s6">
+            <input id="usuario" type="text" class="validate" name='usuario'>
+            <label for="usuario">Usuario</label>    
+         </div>
+         <div class="input-field col s6">
+            <input id="carnet" type="text" class="validate" name='carnet'>
+            <label for="carnet">Carnet</label>
+         </div>
+      </div>
+      <div class="row">
+         <div class="input-field col s6">
+            <input id="grado" type="text" class="validate" name='grado'>
+            <label for="grado">Grado</label>
+         </div>
+         <div class="input-field col s6">
+            <input id="especialidad" type="text" class="validate" name='especialidad'>
+            <label for="especialidad">Especialidad</label>
+         </div>
+      </div>
+      <!--BOTON-->
+      <div class="row">
+      <div class="col offset-l4 l11 offset-s1">
+         <button class="waves-effect waves-light btn sg2 boton-editar2 offset-l4" type='submit' name='editar'> Editar información </button>
+      </div>
+      </div>
+   </form>
+</div>
+</div>
+
+
+ 
--- app/views/public/templates/page.class.php
+++ app/views/public/templates/page.class.php
@@ -15,7 +15,7 @@ class Page extends component{
             <link type='text/css' rel='stylesheet' href='../../web/css/material_icons.css'/>
             <link type='text/css' rel='stylesheet' href='../../web/css/material_icons.css'/>
             <link type='text/css' rel='stylesheet' href='../../web/css/menu.css'/>
-            <link type='text/css' rel='stylesheet' href='../../web/css/citas.css'/>
+            <link type='text/css' rel='stylesheet' href='../../web/css/becados.css'/>
             <script type='text/javascript' src='../../web/js/sweetalert.min.js'></script>
             <meta name='viewport' content='width=device-width, initial-scale=1.0'/>
         </head>
