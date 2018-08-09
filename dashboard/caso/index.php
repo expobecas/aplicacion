2654e66c27e5e749d7c43e9087ace4bb3c59995b
expobecas <37518924+expobecas@users.noreply.github.com>
2018-08-08 19:28:04 -0600

    GUIUUUU

--- dashboard/caso/index.php
+++ dashboard/caso/index.php
@@ -1,6 +1,6 @@
 <?php
 require_once("../../app/views/dashboard/templates/page.class.php");
 Page::templateHeader("Menu");
-require_once("../../app/views/dashboard/casos/index_view.php");
+require_once("../../app/views/dashboard/caso/index_view.php");
 Page::templateFooter();
 ?>
